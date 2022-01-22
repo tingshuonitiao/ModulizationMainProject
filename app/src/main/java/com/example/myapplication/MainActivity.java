9880b04b8f6cb5ce0a04ec82eb6fe3757338151d
zhangxiaozong <zhangxiaozong@bytedance.com>
2022-01-22 16:43:59 +0800

    1.add new module happy_api 2.upload happy_api aar to local maven 3.switch aar and source in the same directory as main project 4.switch aar and source in a different directory than main project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.happy_api.IHappyApi;
+import com.example.modulizationsubprojecta.CommonTextView;
+import com.example.modulizationsubprojecta.ToastUtil;
 import com.ss.android.ugc.base.ServiceManager;
 import com.ss.android.ugc.enterprise_api.IEnterpriseApi;
 import com.ss.android.ugc.poi_api.IPoiApi;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +20,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.init_enterprise).setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
                 ServiceManager.INSTANCE.getService(IEnterpriseApi.class).init();
             }
         });
         findViewById(R.id.init_poi).setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
                 ServiceManager.INSTANCE.getService(IPoiApi.class).init();
+            }
+        });
+        findViewById(R.id.init_happy).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ServiceManager.INSTANCE.getService(IHappyApi.class).haveFunTogether();
+            }
+        });
+        findViewById(R.id.init_common_ui).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CommonTextView textView = new CommonTextView(MainActivity.this);
+                textView.setText("test");
+            }
+        });
+        findViewById(R.id.init_common_util).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtil.showToast(MainActivity.this, "hello, I'm using common util");
             }
         });
     }
---
+++ happy_api/src/main/java/com/example/happy_api/IHappyApi.kt
@@ -0,0 +1,9 @@
+package com.example.happy_api
+
+interface IHappyApi {
+
+    /**
+     *  have fun together
+     */
+    fun haveFunTogether()
+}
